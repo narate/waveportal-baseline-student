b2c0c5179aa5dfa6a14b88ae4585ca1ec03b9554
Narate Ketram <koonnarate@gmail.com>
2021-09-14 18:49:49 +0700

    Add wave button call wave() in smart contract

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/WavePortal.json';
 
 export default function App() {
-
   const [currentAccount, setCurrentAccount] = React.useState("")
+  const contractAddress = '0xDA5f2139A79243c75BCebA2756f784Fa89152B46'
+  const contractABI = abi.abi
+
+  const [waves, setWaves] = React.useState(0)
 
   const checkIfWalletIsConnected = () => {
     const { ethereum } = window;
@@ -42,11 +46,43 @@ export default function App() {
       .catch(err => console.log(err));
   }
 
+  // Get waved count
+  (async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const waveportalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    let count = await waveportalContract.getTotalWaves()
+    console.log('Initial value..., total waves = ' + count.toNumber())
+    setWaves(count.toNumber())
+  })();
+
+  const wave = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const waveportalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    let count = await waveportalContract.getTotalWaves()
+    console.log("Retrived total wave count...", count.toNumber())
+
+    const waveTxn = await waveportalContract.wave(1);
+    console.log("Mining...", waveTxn.hash)
+    await waveTxn.wait()
+    console.log("Mined -- ", waveTxn.hash)
+
+    count = await waveportalContract.getTotalWaves();
+    console.log("Retrived total wave count...", count.toNumber())
+    setWaves(count.toNumber())
+  }
+
   React.useEffect(() => {
     checkIfWalletIsConnected()
   }, [])
 
-  const wave = () => {
+  function WaveAtMe() {
+    return <button className="waveButton" onClick={wave}>
+      Wave at Me 👋
+    </button>
   }
 
   return (
@@ -54,20 +90,21 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-          Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-          Narate,from Thailand.
+          Narate, from Thailand 🇹🇭
+        </div>
+        <div className="bio">
+          <h1>Total waves {waves}</h1>
         </div>
 
-        <button className="waveButton" onClick={null}>
-          Wave at Me
-        </button>
-
+        <WaveAtMe />
+        
         {currentAccount ? null : (
           <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
+            Connect Wallet 🔗
           </button>
         )}
       </div>
